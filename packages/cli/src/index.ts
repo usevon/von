77e08b5f4e34dc85a3715e0ace17691cdc64d1cc
Kyle <kgrahammatzen@gmail.com>
2025-11-30 12:57:36 -0500

    feat(backend): add von status command to CLI

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -3,6 +3,7 @@ import { Command } from "commander"
 import { login } from "@/commands/login"
 import { logout } from "@/commands/logout"
 import { switchOrg } from "@/commands/switch"
+import { status } from "@/commands/status"
 import { dev } from "@/commands/dev"
 
 const program = new Command("von")
@@ -12,6 +13,7 @@ const program = new Command("von")
 program.addCommand(login)
 program.addCommand(logout)
 program.addCommand(switchOrg)
+program.addCommand(status)
 program.addCommand(dev)
 
 program.parse()
