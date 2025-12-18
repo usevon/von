2a9ce004fc1d76d27b2c752477c5176f4328aed1
Kyle <kgrahammatzen@gmail.com>
2025-12-18 15:24:03 -0500

    feat(backend): add Treaty support to CLI tunnel client

--- apps/tunnel/src/index.ts
+++ apps/tunnel/src/index.ts
@@ -2,6 +2,8 @@ import { logger } from "@usevon/utils/logger";
 import { app } from "@/app";
 import { env } from "@/env";
 
+export type { App } from "@/app";
+
 app.listen(env.PORT, () => {
-  logger.info(`Tunnel server running on http://localhost:${env.PORT}`);
+	logger.info(`Tunnel server running on http://localhost:${env.PORT}`);
 });
