c4668a5ab5d18614d3297ec7aeae01b51e383671
Kyle <kgrahammatzen@gmail.com>
2025-12-20 15:16:07 -0500

    feat(frontend): migrate UI components to @base-ui/react v1.0.0

--- packages/ui/src/components/ui/dialog.tsx
+++ packages/ui/src/components/ui/dialog.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Dialog as DialogPrimitive } from "@base-ui-components/react/dialog";
+import { Dialog as DialogPrimitive } from "@base-ui/react/dialog";
 import { XIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
