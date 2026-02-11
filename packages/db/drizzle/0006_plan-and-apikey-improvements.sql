f397dd0dca9cde7cd5469468cfd0378302427a41
Kyle <kgrahammatzen@gmail.com>
2026-02-11 12:19:36 -0500

    chore: remove manual migration in favor of generated migration

--- packages/db/drizzle/0006_plan-and-apikey-improvements.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "organization" ADD COLUMN "plan" text NOT NULL DEFAULT 'hobby';
-ALTER TABLE "apikey" ALTER COLUMN "scopes" TYPE jsonb USING scopes::jsonb;
-DROP INDEX IF EXISTS "apikey_key_idx";
-CREATE UNIQUE INDEX "apikey_key_uidx" ON "apikey" ("key");
