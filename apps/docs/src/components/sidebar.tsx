13ec2f97756201312abe59a666b860c5fbf480ad
Kyle Graham Matzen <kgrahammatzen@gmail.com>
2026-02-12 22:38:03 -0500

    fix: import docs navigation in sidebar

--- apps/docs/src/components/sidebar.tsx
+++ apps/docs/src/components/sidebar.tsx
@@ -11,6 +11,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { siteUrl } from "@/lib/urls";
 import { Search } from "./docs/search";
+import { Navigation } from "./docs/navigation";
 
 export const Sidebar = () => (
   <aside className="fixed top-0 hidden h-svh w-64 border-r bg-[color-mix(in_srgb,var(--color-background),var(--color-foreground)_2%)] lg:block dark:bg-[color-mix(in_srgb,var(--color-background),white_2%)]">
